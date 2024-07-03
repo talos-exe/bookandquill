f6afe1dfcf518500c1473794c15b328e48bc0e65
walterdevk <69842124+walterdevk@users.noreply.github.com>
2024-07-03 05:08:49 -0500

    initial commit

---
+++ CustomStickyNotes/CustomStickyNotes/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CustomStickyNotes
+{
+    public partial class Form1 : Form
+    {
+        private SoundPlayer menuClick = new SoundPlayer("uiclick.wav");
+        Label newLabel;
+        bool newJournalClicked = false;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            //this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+
+        {
+            if (!newJournalClicked)
+            {
+                DialogResult dialog = MessageBox.Show("Are you sure you want to quit?", "Quit", MessageBoxButtons.YesNo);
+                if (dialog == DialogResult.Yes)
+                {
+                    Application.ExitThread();
+                }
+                else if (dialog == DialogResult.No)
+                {
+                    e.Cancel = true;
+                }
+            }
+            
+        }
+
+        private void expandButtons(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void mouseEnterToolStripLabel(object sender, EventArgs e)
+        {
+            Label getLabel = (Label)sender;
+            getLabel.ForeColor = System.Drawing.Color.Green;
+        }
+
+        private void mouseLeaveToolStripLabel(object sender, EventArgs e)
+        {
+            Label getLabel = (Label)sender;
+            getLabel.ForeColor = System.Drawing.Color.Black;
+        }
+
+        private void promptForQuit(object sender, EventArgs e)
+        {
+            menuClick.Play();
+            if (MessageBox.Show("Are you sure you want to quit?", "Quit", MessageBoxButtons.YesNo) == System.Windows.Forms.DialogResult.Yes)
+            {
+                Application.ExitThread();
+            }
+        }
+
+        private void openNewJournal(object sender, EventArgs e)
+        {
+            menuClick.Play();
+            newJournalClicked = true;
+            Form2 journalForm = new Form2();
+            journalForm.Show();
+            this.Hide(); // CHANGE TO CLOSE IN COMPILE
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ CustomStickyNotes/CustomStickyNotes/Form2.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.Media;
+
+namespace CustomStickyNotes { 
+
+    public partial class Form2 : Form
+    {
+
+        private SoundPlayer flipPageOne;
+        private SoundPlayer flipPageTwo;
+        private SoundPlayer flipPageThree;
+        private CustomRTB _customRTB = new CustomRTB();
+        private bool _customRTBTextChanged = false;
+
+        
+        public Form2()
+        {
+            InitializeComponent();
+            this.Controls.Add(_customRTB);
+            DoubleBuffered = true;
+            KeyPreview = true;
+            nextPageBtn.BackgroundImage = (System.Drawing.Image)(Properties.Resources.nextpagebtn);
+            backPageBtn.BackgroundImage = (System.Drawing.Image)(Properties.Resources.nextpagebtn2);
+            backPageBtn.Visible = false;
+            flipPageOne = new SoundPlayer("flip1.wav");
+            flipPageTwo = new SoundPlayer("flip2.wav");
+            flipPageThree = new SoundPlayer("flip3.wav");
+        }
+
+
+        private void loadcustomRTB()
+        {
+            _customRTB.Location = new System.Drawing.Point(45, 79);
+            _customRTB.Size = new System.Drawing.Size(340, 378);
+            _customRTB.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            _customRTB.Font = new System.Drawing.Font("Monocraft", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            _customRTB.ForeColor = System.Drawing.SystemColors.ControlText;
+            _customRTB.Name = "customRTB";
+            _customRTB.Text = "";
+        }
+
+        private void customRTBTextChanged(object sender, EventArgs e)
+        {
+            _customRTBTextChanged = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            loadcustomRTB();
+        }
+
+        private void nextPageBtn_mouseLeave(object sender, EventArgs e)
+        {
+            nextPageBtn.BackgroundImage = ((System.Drawing.Image)(Properties.Resources.nextpagebtn));
+        }
+
+        private void nextPageBtn_mouseEnter(object sender, EventArgs e)
+        {
+            nextPageBtn.BackgroundImage = ((System.Drawing.Image)(Properties.Resources.nextpagebtnmouseovert));
+        }
+
+        private void nextPageBtn_Click(object sender, EventArgs e)
+        {
+            flipPageOne.Play();
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void Form2_Closing(object sender, FormClosingEventArgs e)
+        {
+            if (_customRTBTextChanged)
+            {
+                DialogResult dialog = MessageBox.Show("You have unsaved changes. Are you sure you want to quit?", "Quit", MessageBoxButtons.YesNo);
+                if (dialog == DialogResult.Yes)
+                {
+                    Application.ExitThread();
+                }
+                else if (dialog == DialogResult.No)
+                {
+                    e.Cancel = true;
+                }
+            }
+        }
+    }
+
+    
+}
